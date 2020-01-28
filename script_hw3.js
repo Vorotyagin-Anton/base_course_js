03606730da7213a1a178eca61d495af426c7e620
Anton Vorotyagin <a.vorotyagin@yandex.ru>
2020-01-28 21:32:37 +0300

    homework03 - only JS

---
+++ script_hw3.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Задание №1
+
+function task1() {
+
+	let i = 0;
+
+	while(true){
+		console.log(i);
+		i++;
+		if (i > 100)
+			break;
+	}
+
+};
+
+task1();
+
+// Задание №2
+
+let basket = [["Куртка", 100], ["Штаны", 50], ["Футболка", 30]];
+
+function countBasketPrice(arrayOfGoods) {
+
+	let total = 0;
+
+	for (let good = 0; good < arrayOfGoods.length; good++){
+		total += arrayOfGoods[good][1]; 
+	}
+
+	console.log(total);
+	
+};
+
+countBasketPrice(basket);
+
+// Задание №3
+
+for (let i = 0; i < 10; console.log(i++)){}
+
+// Задание №4
+
+function task4(){
+
+	let raw = "x";
+
+	for (let i = 0; i < 20; i++){
+		console.log(raw);
+		raw += "x";
+	}
+
+};
+
+task4();
